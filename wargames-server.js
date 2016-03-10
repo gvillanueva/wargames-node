20fdbc30def03b7c2a46e65752ac30de7654f087
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2016-03-10 06:47:22 -0800

    Begins to separate Game into isolated instances. Adds Game module. Adds games dictionary. Begins game creation RPC. Begins to add user/game object arguments to listen function. Adds player joined/left events.

--- wargames-server.js
+++ wargames-server.js
@@ -2,6 +2,7 @@ var config = require('./config');
 var Events = require('events');
 var Uuid = require('node-uuid');
 var JsonRpc = require('json-rpc2');
+var Game = require('./game');
 
 var eventEmitter = new Events.EventEmitter();
 var server = JsonRpc.Server.$create({
@@ -11,25 +12,37 @@ var server = JsonRpc.Server.$create({
 	}
 });
 
-//var games[];
+var games = {};
 //games contain expiry timeout
 //games contain list of users[];
 //games contain list of messages[]?  probably not necessary
 
 var create = function(args, opts, callback){
-    // User exceeded number of concurrent games?
-    // Game name unique?
+    var user = args[0];
+    var gameName = args[1];
+
+    // Is game name unique?
+    if (games[gameName])
+        callback('game already exists', null);
+
+    // Has user exceeded number of concurrent games?
 
     // Create game
-    //var game = new Game();
-
-    // Set expiration of game (1800000ms = 30m * 60s * 1000ms)
-    //game.timeout = setTimeout(serializeGame, 1800000);
+    var game = new Game(gameName, 5);
+    
+    // Set game timeout
+    //game.timeout = setTimeout(serializeGame, config.game.timeout);
+    
+    // Add game to games object
+    games[gameName] = game;
+    callback(null, game);
 }
 
 var listen = function(args, opts, callback){
-    // userObj
-    // gameObj
+    var user = args[0];
+    var game = args[1];
+
+    //if games[game.name] || games[game.name].user[user.name]
     // if user not in game
     //  return;
     console.log('connection started');
@@ -38,13 +51,27 @@ var listen = function(args, opts, callback){
         opts.call('chat', event.data);
         //opts.call('game.chat', event.data);
     }
+    var joined = function(event){
+        opts.call('joined', event.data);
+    }
+    var left = function(event){
+        opts.call('left', event.data);
+    }
+
+    eventEmitter.emit('joined', {data: args});
 
     // Register events for 'client' peer notifications
     eventEmitter.on('chat', chat);
     //eventEmitter.on('gameName.chat', chat);
+    eventEmitter.on('joined', joined);
+    eventEmitter.on('left', left);
+    
     opts.stream(function(){
         console.log('connection ended');
         eventEmitter.removeListener('chat', chat);
+        eventEmitter.removeListener('joined', joined);
+        eventEmitter.removeListener('left', left);
+        eventEmitter.emit('left', {data: args});
     });
 
     // Leave connection open
@@ -57,7 +84,8 @@ var chat = function(args, opts, callback){
     // messageObj
     // if user not in game
     //  return;
-    console.log(args);
+    
+    //console.log(args);
     eventEmitter.emit('chat', {data: args});
     //eventEmitter.emit(game.name + '.chat', {data: user.name + ": " + message.text});
     //games[game.id].resetTimeout();
@@ -67,6 +95,7 @@ var chat = function(args, opts, callback){
 server.expose('game.create', create)
 server.expose('game.listen', listen);
 server.expose('game.chat', chat);
+//server.expose('game.users', users)
 
 // Finally, listen for incoming connections
 server.listen(8000, config.hostname);
