19306eb597e2611e7d24aabeb6da4136f2de7d84
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2016-05-24 07:29:32 -0700

    Subclass Error for framework-specific errors

---
+++ systems/wargames/error.js
@@ -0,0 +1,14 @@
+/**
+ * @file Defines standard exception types for the Wargames framework
+ * @author Giancarlo Villanueva
+ */
+
+WargamesError.prototype = Object.create(Error);
+WargamesError.prototype.constructor = WargamesError;
+
+function WargamesError(message) {
+    Error.captureStackTrace(this, this.constructor);
+    this.message = message;
+}
+
+module.exports = WargamesError;
